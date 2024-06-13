ff462c7eacaf94502f528b917d14a3a3ac7f8f3a
Khanh nine seven <33818840+khanh97dev@users.noreply.github.com>
2024-06-13 11:35:49 +0700

    Create install-mysql.sh

---
+++ install-mysql.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+echo "install: mysql...\n"
+
+apk add --no-cache mysql mysql-client
+
+echo "init...\n"
+/etc/init.d/mariadb setup
+
+sed -i 's/^skip-networking/#&/' /etc/my.cnf.d/mariadb-server.cnf
+sed -i 's/^bind-address=0.0.0.0/#&/' /etc/my.cnf.d/mariadb-server.cnf
+
+echo "[client]" >> /etc/my.cnf
+echo "user=root" >> /etc/my.cnf
+echo "password=root" >> /etc/my.cnf
+
+if ! command -v openrc &> /dev/null
+then
+  curl -o- https://raw.githubusercontent.com/khanh97dev/alpine/main/install-openrc.sh | bash -x
+fi
+
+rc-update add mariadb
+
+rc-service mariadb restart
