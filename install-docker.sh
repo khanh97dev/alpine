b52548cd6f083b4b7f415c43bce4b8f6aebd87ff
Khanh nine seven <33818840+khanh97dev@users.noreply.github.com>
2024-06-13 11:36:44 +0700

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,24 +1,17 @@
 #!/bin/sh
+
+echo "intatall: docker..."
+
 echo "if networking nor run, pls start 'networking'!\n"
 echo "rc-update add networking\n"
 echo "rc-service networking service\n"
 
-echo "installing..."
 apk add --no-cache docker docker-cli-compose
 
 if ! command -v openrc &> /dev/null
-then
-  echo 'install: openrc ...'
-  apk add openrc
-  if ! -d /run/openrc
-  then
-    mkdir -p /run/openrc
-  fi
-  # handle
-  openrc
-  touch /run/openrc/softlevel
+then 
+  curl -o- https://raw.githubusercontent.com/khanh97dev/alpine/main/install-openrc.sh | bash -x
 fi
 
-echo "add: boot..."
 rc-update add docker
 rc-service docker restart
